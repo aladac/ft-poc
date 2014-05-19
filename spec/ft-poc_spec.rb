c579204446d977b04d2281cb4228940522f46f40
Adam Ladachowski <adam@saiden.pl>
2014-05-19 09:54:50 +0200

    rudimentary tests

---
+++ spec/ft-poc_spec.rb
@@ -0,0 +1,23 @@
+require './lib/ft-poc'
+
+describe FT::Search, ".new" do
+  it "Creates a FM::Search obj" do
+    search = FT::Search.new
+    search.class.should eq(FT::Search)
+  end
+end
+
+describe FT::Search, "#perform" do
+  it "Should fail without at least @phrase" do
+    search = FT::Search.new
+    expect{search.perform}.to raise_error(ArgumentError)
+  end
+end
+
+describe FT::Search, "#prune_links" do
+  it "Should return an Array on success" do
+    search = FT::Search.new
+    search.phrase = "blabla"
+    search.prune_links.class.should eq(Array)
+  end
+end
